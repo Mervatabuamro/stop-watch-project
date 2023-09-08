def3ac3293f00521146d14e81ec97d5d4a02c22c
Mervatamro <mervat.abuamro@gmail.com>
2023-09-08 13:22:07 +0200

    stopwatch updates

--- src/init.js
+++ src/init.js
@@ -1,1 +1,69 @@
 console.log("Let'us build a stop watch");
+
+const timeDisplay = document.getElementById('timeDisplay');
+const startBtn = document.getElementById('button-start');
+const stopBtn = document.getElementById('button-stop');
+const resetBtn = document.getElementById('button-reset');
+const millSeconds = document.getElementById('millseconds');
+const seconds = document.getElementById('seconds');
+const minutes = document.getElementById('mints');
+
+let startTime;
+let intervalId;
+
+// start button
+
+const startTimer = () => {
+    if (!startTime) {
+        startTime = Date.now();
+        intervalId = setInterval(updateTimer, 1000);
+    }
+};
+
+// stop button
+const stopTimer = () => {
+    if (startTime) {
+        clearInterval(intervalId);
+        startTime = null;
+    }
+};
+
+// reset button
+
+const resetTimer = () => {
+    stopTimer();
+    const stopwatchDisplay = document.querySelector('.stopwatch');
+    stopwatchDisplay.innerHTML = '00:00:00';
+};
+
+/*function updateTimer() {
+    if (startTime) {
+        const currentTime = new Date(Date.now() - startTime);
+        const formattedTime = currentTime.toISOString().substr(11, 8);
+        document.getElementById('timeDisplay').innerText = formattedTime;
+    }
+}*/
+
+// updating the stopwatch
+
+let min = 0;
+let sec = 0;
+let msec = 0;
+
+const updateTimer = () => {
+    msec++;
+    millSeconds.innerText = msec;
+    if (msec >= 100) {
+        sec++;
+        seconds.innerText = sec;
+        msec = 0;
+    } else if (sec >= 60) {
+        min++;
+        minutes.innerText = min;
+        sec = 0;
+    }
+};
+
+startBtn.addEventListener('click', startTimer);
+stopBtn.addEventListener('click', stopTimer);
+resetBtn.addEventListener('click', resetTimer);
